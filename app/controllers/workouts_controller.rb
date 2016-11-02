b212399401b4b62d88a1c5ca57a4b71a308b2ae2
tzits <tzitsman@gmail.com>
2016-11-02 16:23:12 -0700

    establish routes for workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,8 @@
 class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+  end
+  def show
+    @workout = Workout.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/",				to: "sessions#new",				as: "login"
 
+  get "/workouts", to: "workouts#index",    as: "workouts"
+  get "/workouts/:id", to: "workouts#show",  as: "workout"
 
-  
 end
