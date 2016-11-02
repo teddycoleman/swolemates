905376851cfee51eb6a531e54690878c3876df5f
tzits <tzitsman@gmail.com>
2016-11-02 16:34:49 -0700

    created index page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,7 +2,4 @@ class WorkoutsController < ApplicationController
   def index
     @workouts = Workout.all
   end
-  def show
-    @workout = Workout.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get "/",				to: "sessions#new",				as: "login"
 
   get "/workouts", to: "workouts#index",    as: "workouts"
-  get "/workouts/:id", to: "workouts#show",  as: "workout"
 
 end
