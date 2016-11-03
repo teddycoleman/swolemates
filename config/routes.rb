646f3fb4284b3c3cb0cbe836c15f1ea0f9352c99
tzits <tzitsman@gmail.com>
2016-11-03 10:59:37 -0700

    removes merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root "sessions#new"
 
-<<<<<<< HEAD
-  get "/",				to: "sessions#new",				as: "login"
-  resources :users
-=======
   resources :users
 
   get "/",					to: "sessions#new"
@@ -16,8 +12,7 @@ Rails.application.routes.draw do
   post "/results", to: "results#create"
   get "/results/:id", to: "results#show", as: 'result'
   get "/challenges", to: "challenges#index", as: "challenges"
-  get "/challenges/new", to: "challenges#new", as: "new_challenge" 
+  get "/challenges/new", to: "challenges#new", as: "new_challenge"
   post "/challenges", to: "challenges#create"
->>>>>>> 3cf82a2e8a371f458afbb93db2282009099a707d
 
 end
