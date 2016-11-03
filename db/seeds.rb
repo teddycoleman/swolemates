a863bb36429d443070aae7149886b0164e0dd060
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-03 10:34:14 -0700

    adds create new challenge functionality and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Workout.create(title: '5 mile run', description: '5 miles straight of running', qty: 5, unit: 'miles', unit_of_competition: 'mins')
+Workout.create(title: '10 mile run', description: '10 miles straight of running', qty: 10, unit: 'miles', unit_of_competition: 'mins')
+Workout.create(title: '10 mile ride', description: '10 miles straight of biking', qty: 10, unit: 'miles', unit_of_competition: 'mins')
+Workout.create(title: '25 mile ride', description: '25 miles straight of biking', qty: 25, unit: 'miles', unit_of_competition: 'mins')
+Workout.create(title: '800 meter swim', description: '.5 miles straight of swimming', qty: 800, unit: 'meters', unit_of_competition: 'mins')
+Workout.create(title: '1 mile swim', description: '1 mile straight of swimming', qty: 1, unit: 'miles', unit_of_competition: 'mins')
+
+User.create(name: 'Toby', email: 'tzitsman@gmail.com', gender: 'Male', age: 25, fav_food: 'pasta', fav_workout: 'long run')
+User.create(name: 'Natalia', email: 'natalia@fruit.com', gender: 'Female', age: 24, fav_food: 'kale', fav_workout: 'all of them')
+
+
+Result.create(user_id: 1, workout_id: 1, qty: 35, unit: 'minutes')
+Result.create(user_id: 1, workout_id: 5, qty: 7, unit: 'minutes')
+Result.create(user_id: 1, workout_id: 4, qty: 75, unit: 'minutes')
+
