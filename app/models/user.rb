5d9119da85185fe91020075f07e9f40c9abad84d
lordshadowist <shadowist@live.com>
2016-11-03 10:51:18 -0700

    User Show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	extend FriendlyId
+  friendly_id :name
 	has_many :results
 	has_many :challenges
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "sessions#new"
 
   get "/",				to: "sessions#new",				as: "login"
-
+  resources :users
 
   
 end
