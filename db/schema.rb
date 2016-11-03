8f42a309bb484974e5dfc7b250aa12db7d87e58d
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-11-03 10:18:38 -0700

    add paperclip gem and update user model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103164629) do
+ActiveRecord::Schema.define(version: 20161103171509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,11 +49,15 @@ ActiveRecord::Schema.define(version: 20161103164629) do
     t.integer  "age"
     t.string   "fav_food"
     t.string   "fav_workout"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "location"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "workouts", force: :cascade do |t|
