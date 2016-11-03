7fb32ff661b30461af8ef9f9117deb9d6f2c5d0e
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-11-03 15:27:34 -0700

    add error and success flash messages for all controllers

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -6,15 +6,26 @@ class ChallengesController < ApplicationController
 		@opponents = User.all
 		@results = current_user.results
 	end
+
 	def index
 		@challenges = current_user.challenges
 	end
+
 	def create
 		challenge = Challenge.create(challenge_params)
-		redirect_to root_path
+		
+		if challenge.save
+			flash[:success] = "New challenge successfully created! Go find your Swolemate!"
+			redirect_to root_path
+		else
+			flash[:error] = "Oops. There was a problem making a new challenge."
+			redirect_to :back
+		end
+
 	end
 
 	private
+
 	def challenge_params
 		params.require(:challenge).permit(:opponent_id, :workout_id, :result_id, :user_id)
 	end
