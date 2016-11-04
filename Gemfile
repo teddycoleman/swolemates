2f2715009da78b8d3ac9d4bffc2ed8630d602179
lordshadowist <shadowist@live.com>
2016-11-04 11:45:33 -0700

    User model tests

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem "paperclip", "~> 5.0.0"
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 # Use Capistrano for deployment
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+	subject(:user) {User.new}
   describe "::new" do
   	it "initializes a new user" do
-  		user = User.new
     	expect(user).to be_a(User)
   	end
   end
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+
+  context "when validating an email" do
+    it "should contain an @ symbol" do
+      user.email = "asdf"
+      expect{user.save!}.to raise_error
+    end
+  end
+
+  it { should have_many(:results) }
+  it { should have_many(:challenges) }
+  it { should validate_inclusion_of(:gender).in_array(["male", "female", "other"]) }
+  it { should validate_numericality_of(:age)}
+  it { should_not validate_numericality_of(:fav_food)}
+  it { should_not validate_numericality_of(:fav_workout)}
+  it { should have_secure_password}
 end
