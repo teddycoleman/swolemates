5d2d6bbb9ab6fe0e7c8da1ddcb2fbb1953c40808
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-11-03 22:43:00 -0700

    add validations for all models

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,4 +4,6 @@
   belongs_to :opponent, class_name: "User"
   belongs_to :result
   belongs_to :opponent_results, class_name: "Result", optional: true
+
+	validates :workout_id, :opponent_id, :result_id, presence: true
 end
