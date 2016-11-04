4788f50f3de1f928b9627288dad46c80e0ff6bf4
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-04 14:05:11 -0700

    adds fix for declaring winner

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,4 @@
-	class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
   belongs_to :workout
   belongs_to :user
   belongs_to :opponent, class_name: "User"
@@ -6,4 +6,14 @@
   belongs_to :opponent_results, class_name: "Result", optional: true
 
 	validates :workout_id, :opponent_id, :result_id, presence: true
+
+	def declare_a_winner
+		return nil if opponent_results.nil?
+		if result.unit === 'mins' 
+			result.qty < opponent_results.qty ? user : opponent
+		else
+			result.qty < opponent_results.qty ? opponent : user
+		end
+	end
+
 end
